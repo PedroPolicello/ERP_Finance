2116e45f8b7b67af1c8b8fc32ae2b8b0f254edff
Pedro Policello <pedro_policello@hotmail.com>
2026-09-03 12:13:15 -0300

    [FEAT] Controller file commit

---
+++ ERP_Finance/Controllers/OrderItemController.cs
@@ -0,0 +1,34 @@
+﻿using ERP_Finance.DTOs.OrderItem;
+using ERP_Finance.Entities;
+using ERP_Finance.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERP_Finance.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrderItemController : ControllerBase
+{
+    private readonly OrderItemService _orderItemService;
+
+    public OrderItemController(OrderItemService orderItemService)
+    {
+        _orderItemService = orderItemService;
+    }
+
+    [HttpGet("{id:guid}")]
+    public ActionResult<OrderItem> GetOrderItem(Guid id)
+    {
+        var orderItem = _orderItemService.GetOrderItemService(id);
+
+        return Ok(orderItem);
+    }
+
+    [HttpPost]
+    public ActionResult CreateOrderItem([FromBody] CreateOrderItemDTO orderItemDTO)
+    {
+        var result = _orderItemService.CreateOrderItemService(orderItemDTO);
+
+        return CreatedAtAction(nameof(GetOrderItem), new { id = result.Id }, result);
+    }
+}
