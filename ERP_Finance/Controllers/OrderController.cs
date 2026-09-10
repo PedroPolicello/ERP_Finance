b013566f738cc9dbd7cc75acba94e2ec755c60d4
Pedro Policello <pedro_policello@hotmail.com>
2026-09-10 11:34:54 -0300

    [FEAT] OrderController
    Summary:"
    - Order Controller added.
    "

---
+++ ERP_Finance/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using ERP_Finance.DTOs.Order;
+using ERP_Finance.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERP_Finance.Controllers;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrderController : ControllerBase
+{
+    private readonly OrderService _orderService;
+
+    public OrderController(OrderService orderService)
+    {
+        _orderService = orderService;
+    }
+
+    [HttpGet("{id:guid}")]
+    public ActionResult GetOrder(Guid id)
+    {
+        var order = _orderService.GetOrderService(id);
+
+        return Ok(order);
+    }
+
+    [HttpPost]
+    public ActionResult CreateOrder([FromBody] CreateOrderDTO orderDTO)
+    {
+        var result = _orderService.CreateOrderService(orderDTO);
+
+        return CreatedAtAction(nameof(GetOrder), new { id = result.Id }, result);
+    }
+}
